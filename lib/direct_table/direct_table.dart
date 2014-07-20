dd252a8ef1a267f2d64bb574de28eed2395da432
Arubaruba <shadowstep7@gmail.com>
2014-07-20 17:24:45 +0300

    direct_table -> Editable

--- lib/direct_table/direct_table.dart
+++ lib/direct_table/direct_table.dart
@@ -17,9 +17,10 @@ class DirectTable {
 
   String couchDbQuery = '';
 
-  @NgOneWay('documents')
   List<dynamic> documents = [];
   List<String> fieldNames = [];
+  Map<String, dynamic> additionalDocument = {'doc':{}};
+  bool readOnly = false;
 
   DirectTable(this.http, this.scope) {
   }
@@ -43,17 +44,73 @@ class DirectTable {
     }
   }
 
-  void loadDocuments({int amount: 10}) {
-    http.get(couchDbQuery, params: {'include_docs': true}).then((HttpResponse result) {
+  void loadDocuments({String id: '_all_docs'}) {
+    http.get(couchDbQuery + '/$id', params: {'include_docs': true}).then((HttpResponse result) {
       //The Json file is automatically parsed in result.responseText
       if (result != null) {
-        documents.addAll(result.responseText['rows']);
+        if (id.startsWith('_all_docs')) {
+          documents.addAll(result.responseText['rows']);
+        } else {
+          dynamic existingDocument = documents.firstWhere(((dynamic document) => document['doc']['_id'] == id), orElse: (){
+            documents.add({'doc': result.responseText});
+          });
+          if (existingDocument != null) {
+            existingDocument['doc'] = result.responseText;
+          }
+        }
       } else {
         print('Direct Table failed to parse documents [${documents.length} - ${documents.length + amount - 1}] from "$couchDbQuery"');
       }
     }).catchError((error) {
       print(error);
       print('Direct Table failed to load documents [${documents.length} - ${documents.length + amount - 1}] from "$couchDbQuery"');
+    });
+  }
+
+  void addDocument() {
+    additionalDocument['doc']['created'] = new DateTime.now().millisecondsSinceEpoch;
+    additionalDocument['processing'] = true;
+
+    http.get(Config.couchDb + '_uuids?count=1').then((HttpResponse response) {
+
+      String id = response.responseText['uuids'][0];
+
+      http.put(couchDbQuery + '/' + id,
+       JSON.encode(additionalDocument['doc']), headers:{'Content-Type':'application/json'}).then((HttpResponse response) {
+        print(response);
+        additionalDocument['processing'] = false;
+        loadDocuments(id: id);
+      }).catchError((error) {
+        print(error);
+        additionalDocument['processing'] = false;
+      });
+
+    }).catchError((error) {
+      print(error);
+      additionalDocument['processing'] = false;
+    });
+  }
+
+  void backupDocument(Map<String, dynamic> document) {
+    document['doc_backup'] = new Map.from(document['doc']);
+  }
+
+  void restoreDocument(Map<String, dynamic> document) {
+    document['doc'] = document['doc_backup'];
+  }
+
+  void editDocument(dynamic document) {
+    document['processing'] = true;
+    String id = document['doc']['_id'];
+    http.put(couchDbQuery + '/' + id,
+    JSON.encode(document['doc']), headers:{'Content-Type':'application/json'}).then((HttpResponse response) {
+      print(response);
+      document['processing'] = false;
+      document['edit'] = false;
+      loadDocuments(id: id);
+    }).catchError((error) {
+      print(error);
+      document['processing'] = false;
     });
   }
 }
